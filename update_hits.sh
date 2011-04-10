074886e0daa310afeb97a9419736b1095199beef
Rodolfo Carvalho <rhcarvalho@gmail.com>
2011-04-10 13:24:50 -0500

    Change shell script to work under cron environment

--- update_hits.sh
+++ update_hits.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
-python2.7 eskahits.py 400 > ../eskahits_log/hits
-bzr ci --unchanged -m "Hits update - $(date)" ../eskahits_log/hits
+cmd="/usr/local/bin/python2.7 /home/rodolfo/eskahits/eskahits.py 400"
+hits="/home/rodolfo/eskahits_log/hits"
+$cmd > $hits
+/home/rodolfo/bin/bzr ci --unchanged -m "Hits update - $(date)" $hits
