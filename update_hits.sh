ea8b21f08d25ca98445b6bbea777f78b0b21e619
Rodolfo Carvalho <rhcarvalho@gmail.com>
2011-06-14 22:27:02 -0300

    Fetch many more hits (now there are 1122 available)

--- update_hits.sh
+++ update_hits.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-cmd="/usr/local/bin/python2.7 /home/rodolfo/eskahits/eskahits.py 400"
+cmd="/usr/local/bin/python2.7 /home/rodolfo/eskahits/eskahits.py 1400"
 hits="/home/rodolfo/eskahits_log/hits"
 $cmd > $hits
 /home/rodolfo/bin/bzr ci --unchanged -m "Hits update - $(date)" $hits
